73b268b6e0798e0610c09a091704139130b27a10
EC2 Default User <ec2-user@ip-172-31-8-243.ec2.internal>
2019-03-12 21:36:34 +0000

    edit h5_info to make it more general

--- h5_info.py
+++ h5_info.py
@@ -4,17 +4,32 @@ import sys
 def print_attrs(name, obj):
     print name, len(obj), obj
     if name[-6:] == "/shape":
-        print obj[0],obj[1]
+        print name, "shape", obj[0],obj[1]
+
+def example_func(name, obj):
+    if isinstance(obj, h5py.Dataset): # obj is a dataset
+        print name, len(obj), obj[:5]
 
 def get_h5_info (h5_file):
     hF = h5py.File(h5_file)
-    print hF.keys()
+    print "name:", hF.name
+    print "keys:", hF.keys()
+
+    print 
+    print "attributes"
+    hF.visititems(print_attrs)
+
+    print
+    print "dataset example" 
+    hF.visititems(example_func)
+
+    '''
     assembly = hF.keys()[0]
     print assembly
-    hF.visititems(print_attrs)
     print "data :5", hF[assembly+"/data"][:5]
     print "indices :5", hF[assembly+"/indices"][:5]
     print "indptr :3", hF[assembly+"/indptr"][0:3]
+    '''
 
 if __name__ == "__main__" and len(sys.argv[:])!=2:
     print "pyton h5_xena_T.py h5file"
