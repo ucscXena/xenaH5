de18d09f6367f5d4167cec1de35d4608d0631d9c
jingchunzhu <jingchunzhu@gmail.com>
2019-04-10 14:24:44 -0700

    edit

--- 10x_h5_merge.py
+++ 10x_h5_merge.py
@@ -24,7 +24,7 @@ def same(genes, this_genes):
 			return False
 	return True
 
-def addH5file(h5file, group, g):
+def addH5file(h5file, group, g, counter_data, counter_indptr):
 	hF = h5py.File(h5file)
 
 	this_indptr = hF[group +"/indptr"]
@@ -37,42 +37,32 @@ def addH5file(h5file, group, g):
 	rowN = this_shape[0]
 	colN = this_shape[1]
 
-	if len(data) == 0:
-		genes = this_genes
-		gene_names = this_gene_names
-		indptr = this_indptr
-		data = this_data
-		indices = this_indices
-		barcodes = this_barcodes
-		return data, indices, indptr, genes, gene_names, barcodes
-
+	if len(g['data']) == 0:
+		g.create_dataset('genes', data = this_genes, compression="gzip")
+		g.create_dataset('gene_names', data = this_gene_names, compression="gzip")
+    
+	'''
 	else:
 		# check genes
-		'''
-		if not same(genes, this_genes):
-			print h5file, "bad genes, skip"
-			return data, indices, indptr, genes, gene_names, barcodes
-
+		if not same(g['genes'], this_genes):
+			print h5file, "different genes, skip"
+			return
 		print "chk"
-		'''
+    '''
+	#the standard CSC representation
+    #where the row indices for column i are stored in indices[indptr[i]:indptr[i+1]] and
+    #their corresponding values are stored in data[indptr[i]:indptr[i+1]].
+    #If the shape parameter is not supplied, the matrix dimensions are inferred from the index arrays.
 
-		#the standard CSC representation
-	    #where the row indices for column i are stored in indices[indptr[i]:indptr[i+1]] and
-	    #their corresponding values are stored in data[indptr[i]:indptr[i+1]].
-	    #If the shape parameter is not supplied, the matrix dimensions are inferred from the index arrays.
+	# indptr
+	
+	indptr_offset = g['indptr'][counter_indptr]
+	for i in range (0, len(this_indptr)):
+		g['indptr'][counter_indptr + i] = this_indptr[i] + indptr_offset
+	print "indptr offset", indptr_offset
+	counter_indptr = counter_indptr + len(this_indptr)
 
-
-		indptr_offset = indptr[-1]
-		offset_indptr = map(lambda x: x+ indptr_offset, this_indptr)
-		print "indptr offset", indptr_offset
-
-		indptr.extend(offset_indptr[1:])
-
-		barcodes.extend(this_barcodes)
-		print "bar"
-		
-		data.extend(this_data)
-		indices.extend(this_indices)
+	return counter_data,  counter_indptr
 
 def getSizeH5file(h5file, group):
 	hF = h5py.File(h5file)
@@ -96,7 +86,6 @@ for root, dirs, files in os.walk(h5filedir):
 	for file in files:
 		if file == namepatten:
 			h5file = root + '/' +  file
-			count = count +1
 			this_size_indptr, this_size_data = getSizeH5file(h5file, group)
 			size_data = size_data + this_size_data
 			size_indptr = size_indptr + this_size_indptr - 1
@@ -110,16 +99,18 @@ g.create_dataset('indptr', (size_indptr,), dtype='i8', compression="gzip")
 g.create_dataset('data', (size_data,), dtype='i4', compression="gzip")
 g.create_dataset('indices', (size_data,), dtype='i8', compression="gzip")
 g.create_dataset('barcodes', (size_indptr-1,),  dtype='S18', compression="gzip")
-
-print g['data']
-sys.exit()
+g['indptr'][0] = 0
 
 count = 0
+counter_data = 0
+counter_indptr = 0
 for root, dirs, files in os.walk(h5filedir):
 	for file in files:
 		if file == namepatten:
 			h5file = root + '/' +  file
 			count = count +1
-			data, indices, indptr, genes, gene_names, barcodes = addH5file(h5file, group, g)
+			counter_data,  counter_indptr = addH5file(h5file, group, g, counter_data, counter_indptr)
+			if count == 2:
+				sys.exit()
 
-output_h5 (output, group, data, indices, indptr, shape, genes, gene_names, barcodes)
+#output_h5 (output, group, data, indices, indptr, shape, genes, gene_names, barcodes)
