38a49ce6d71d2d875d5f7cb9a87c1271c5a7bcd6
jingchunzhu <jingchunzhu@gmail.com>
2019-04-10 11:19:55 -0700

    add

---
+++ 10x_h5_merge.py
@@ -0,0 +1,13 @@
+import h5py
+import os.sys
+
+if __name__ == "__main__" and len(sys.argv[:])!=2:
+    print "pyton 10x_h5_merge.py dir"
+    sys.exit()
+
+h5filedir = sys.argv[1]
+for root, dirs, files in os.walk(h5filedir):
+	for file in files:
+		ext =os.path.splitext(file)[1]
+		if ext == 'h5':
+			print file
