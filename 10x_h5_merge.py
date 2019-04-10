9e4d065d63564f7470a95dd60abbb880154fdee7
jingchunzhu <jingchunzhu@gmail.com>
2019-04-10 11:26:58 -0700

    edit

--- 10x_h5_merge.py
+++ 10x_h5_merge.py
@@ -1,13 +1,13 @@
 import h5py
 import os, sys
 
-if __name__ == "__main__" and len(sys.argv[:])!=2:
-    print "pyton 10x_h5_merge.py dir"
+if __name__ == "__main__" and len(sys.argv[:])!= 3:
+    print "pyton 10x_h5_merge.py inputdir namepatten(e.g filtered_gene_bc_matrices_h5.h5)"
     sys.exit()
 
 h5filedir = sys.argv[1]
+namepatten = sys.argv[2]
 for root, dirs, files in os.walk(h5filedir):
 	for file in files:
-		ext =os.path.splitext(file)[1]
-		if ext == '.h5':
+		if file == namepatten:
 			print file
